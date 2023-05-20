a55d500d0a336d3d1d33049c156adb44ea5bbdeb
Nabil Anugerah <nabilpangestu123@gmail.com>
2023-05-20 09:56:55 +0700

    add new font

--- src/components/ListTodo.jsx
+++ src/components/ListTodo.jsx
@@ -3,9 +3,9 @@ import { FiTrash } from "react-icons/fi";
 
 function ListTodo() {
   return (
-    <main className="text-center md:text-center inline ">
+    <main className="font-sansPro text-center md:text-center inline ">
       {/* START: Filter Button */}
-      <section className="flex sm:flex justify-center sm:justify-start md:justify-center items-center  md:items-center sm:items-center mx-0 sm:mx-[27rem] md:mx-auto mt-4 sm:mt-2 md:mt-2 lg:mt-2">
+      <section className="font-sansPro flex sm:flex justify-center sm:justify-start md:justify-center items-center  md:items-center sm:items-center mx-0 sm:mx-[27rem] md:mx-auto mt-4 sm:mt-2 md:mt-2 lg:mt-2">
         <button className="px-3 py-2 sm:px-4 sm:py-2 me-3 sm:me-4 bg-green-500 text-white rounded-full font-bold">
           ALL
         </button>
@@ -19,7 +19,7 @@ function ListTodo() {
       {/* END: Filter Button */}
 
       {/* START: List Todo */}
-      <section className="w-[90%] md:w-[70%] lg:w-[40%] flex justify-between items-center mt-8 md:mt-6 lg:mt-6 mx-auto lg:mx-auto md:mx-auto border-[3px] border-gray-700 rounded px-2 py-3">
+      <section className="w-[90%] md:w-[70%] lg:w-[39%] flex justify-between items-center mt-8 md:mt-6 lg:mt-6 mx-auto lg:mx-auto md:mx-auto bg-slate-50 shadow rounded px-3 py-3">
         <div className="flex justify-center items-center">
           <input
             className="w-7 h-5 sm:w-9 sm:h-8"
