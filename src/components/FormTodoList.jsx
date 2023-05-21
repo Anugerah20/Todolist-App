e9cb49c296ef32064e52801e8a6573236c74b773
Nabil Anugerah <nabilpangestu123@gmail.com>
2023-05-21 12:31:49 +0700

    🛒 import usestate

--- src/components/FormTodoList.jsx
+++ src/components/FormTodoList.jsx
@@ -1,17 +1,39 @@
+import { useDispatch } from "react-redux";
+import { useState } from "react";
+import { addTodoList } from "../redux/reducers/todoListReducer";
+
 function FormTodoList() {
+  const dispatch = useDispatch();
+  const [inputTodoList, setInputTodoList] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // console.log(inputTodoList);
+
+    const newTodoList = {
+      id: 4,
+      title: inputTodoList,
+      completed: false,
+    };
+
+    dispatch(addTodoList(newTodoList));
+  };
   return (
     <main className="w-[90%] sm:w-max h-[50vh] sm:h-[50vh] lg:h[50vh] mx-auto md:mx-auto flex sm:flex justify-center sm:justify-center items-center sm:items-center font-sansPro flex-col">
       <h1 className="text-center sm:text-center text-3xl sm:text-4xl font-black">
         What the plan for today?
       </h1>
       <section className="flex sm:flex justify-center sm:justify-center items-center sm:items-start w-full mx-4 sm:mx-auto mt-10 sm:mt-10 md:mt-5 lg:mt-10">
-        <form>
+        <form onSubmit={handleSubmit}>
           <input
             className="w-full sm:w-[450px] px-2 py-2 outline-none border-2 border-violet-600 rounded-sm caret-violet-600 invalid:border-red-600"
             type="text"
             name="todo"
             id="todo"
             placeholder="What to do"
+            value={inputTodoList}
+            onChange={(e) => setInputTodoList(e.target.value)}
             required
           />
           <button
