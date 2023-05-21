8dece79c6303e6d8726d5081d3d84c0de4e92914
Nabil Anugerah <nabilpangestu123@gmail.com>
2023-05-21 13:06:58 +0700

    ✔️ export update and remove

--- src/redux/actions/todoListAction.js
+++ src/redux/actions/todoListAction.js
@@ -1,4 +1,6 @@
 export const ADD_TODO_LIST = "ADD_TODO_LIST";
+export const UPDATE_TODO_LIST = "UPDATE_TODO_LIST";
+export const REMOVE_TODO_LIST = "REMOVE_TODO_LIST";
 
 export const addTodoList = (payload) => {
   return {
