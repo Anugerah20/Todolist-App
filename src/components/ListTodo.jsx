51c36cc6e92704712f624ccdda6e4319bec53024
Nabil Anugerah <nabilpangestu123@gmail.com>
2023-05-21 12:34:04 +0700

    🔍 show data todo

--- src/components/ListTodo.jsx
+++ src/components/ListTodo.jsx
@@ -1,15 +1,18 @@
 import { FiEdit2 } from "react-icons/fi";
 import { FiTrash } from "react-icons/fi";
+import { useSelector } from "react-redux";
 
 function ListTodo() {
+  const { todos } = useSelector((state) => state);
+
   return (
-    <main className="font-sansPro text-center md:text-center inline ">
+    <main className="font-sansPro text-center md:text-center">
       {/* START: Filter Button */}
-      <section className="font-sansPro flex sm:flex justify-center sm:justify-start md:justify-center items-center  md:items-center sm:items-center mx-0 sm:mx-[27rem] md:mx-auto mt-4 sm:mt-2 md:mt-2 lg:mt-2">
-        <button className="px-3 py-2 sm:px-4 sm:py-2 me-3 sm:me-4 bg-green-500 text-white rounded-full font-bold">
+      <section className="font-sansPro flex flex-wrap justify-center sm:justify-start md:justify-center items-center mx-auto mt-4 sm:mt-2 md:mt-2 lg:mt-2">
+        <button className="px-3 py-2 sm:px-4 sm:py-2 me-3 sm:me-4 bg-green-500 text-white rounded-full font-bold mb-2 sm:mb-0">
           ALL
         </button>
-        <button className="px-3 py-2 sm:px-4 sm:py-2 me-3 sm:me-4 bg-slate-500 text-white rounded-full font-bold">
+        <button className="px-3 py-2 sm:px-4 sm:py-2 me-3 sm:me-4 bg-slate-500 text-white rounded-full font-bold mb-2 sm:mb-0">
           ACTIVE
         </button>
         <button className="px-3 py-2 sm:px-4 sm:py-2 bg-slate-500 text-white rounded-full font-bold">
@@ -19,22 +22,22 @@ function ListTodo() {
       {/* END: Filter Button */}
 
       {/* START: List Todo */}
-      <section className="w-[90%] md:w-[70%] lg:w-[39%] flex justify-between items-center mt-8 md:mt-6 lg:mt-6 mx-auto lg:mx-auto md:mx-auto bg-slate-50 shadow rounded px-3 py-3">
-        <div className="flex justify-center items-center">
-          <input
-            className="w-7 h-5 sm:w-9 sm:h-8 cursor-pointer"
-            type="checkbox"
-            name="todo"
-            id="todo"
-          />
-        </div>
-        <div className="flex items-center text-xl sm:text-2xl">
-          <p>Buy Milk</p>
-        </div>
-        <div className="flex items-center gap-4 cursor-pointer">
-          <FiEdit2 className="text-xl sm:text-2xl" />
-          <FiTrash className="text-xl sm:text-2xl" />
-        </div>
+      <section className="w-[90%] md:w-[70%] lg:w-[39%] flex flex-col gap-3 mx-auto mt-5 md:mt-6 lg:mt-6 bg-slate-50 shadow rounded px-4 py-4">
+        {todos.map((item) => (
+          <div key={item.id} className="flex items-center mb-4 p-2 border-2">
+            <input
+              className="w-7 h-5 sm:w-9 sm:h-8 cursor-pointer"
+              type="checkbox"
+              name="todo"
+              id="todo"
+            />
+            <p className="text-xl sm:text-2xl ml-2">{item.title}</p>
+            <div className="flex items-center ml-auto gap-4 cursor-pointer">
+              <FiEdit2 className="text-xl sm:text-2xl" />
+              <FiTrash className="text-xl sm:text-2xl" />
+            </div>
+          </div>
+        ))}
       </section>
       {/* END: List Todo */}
     </main>
