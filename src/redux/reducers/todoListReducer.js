9afee1658f45802235a4f65f3c93a7359158b27e
Nabil Anugerah <nabilpangestu123@gmail.com>
2023-05-22 15:18:44 +0700

    ➕ import  checkbox

--- src/redux/reducers/todoListReducer.js
+++ src/redux/reducers/todoListReducer.js
@@ -1,3 +1,5 @@
+import { ADD_TODO_LIST, UPDATE_CHECKBOX } from "../actions/todoListAction";
+
 const initialState = {
   todos: [
     { id: 1, title: "Buy Burger", completed: false },
@@ -5,8 +7,6 @@ const initialState = {
     { id: 3, title: "Go to Bandung", completed: true },
   ],
 };
-
-export const ADD_TODO_LIST = "ADD_TODO_LIST";
 
 export function addTodoList(newTodoList) {
   return {
@@ -21,9 +21,39 @@ const todoListReducer = (state = initialState, action) => {
       return {
         todos: [...state.todos, action.payload],
       };
+
+    case UPDATE_CHECKBOX:
+      let todoCheckbox = [];
+      state.map((item) => {
+        if (item.id === action.payload) {
+          item.completed = !item.completed;
+        }
+        todoCheckbox.push(item);
+      });
+      return todoCheckbox;
     default:
       return state;
   }
 };
 
 export default todoListReducer;
+
+/* Test */
+// import { ADD_TODO_LIST } from "../actions/todoListAction";
+
+// const initialState = [
+//   { id: 1, title: "Buy Burger", completed: false },
+//   { id: 2, title: "Learn Redux React", completed: false },
+//   { id: 3, title: "Go to Bandung", completed: true },
+// ];
+
+// export const todoListReducer = (state = initialState, action) => {
+//   switch (action.type) {
+//     case ADD_TODO_LIST:
+//       return [...state, action.payload];
+//     default:
+//       return state;
+//   }
+// };
+
+// export default todoListReducer;
