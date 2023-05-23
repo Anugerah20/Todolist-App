0deafb1c3dd0ade9f7f574de2f384f8061a2768e
Nabil Anugerah <nabilpangestu123@gmail.com>
2023-05-23 08:03:45 +0700

    update export

--- src/redux/actions/todoListAction.js
+++ src/redux/actions/todoListAction.js
@@ -1,18 +1,25 @@
 export const ADD_TODO_LIST = "ADD_TODO_LIST";
-export const UPDATE_TODO_LIST = "UPDATE_TODO_LIST";
 export const REMOVE_TODO_LIST = "REMOVE_TODO_LIST";
+export const UPDATE_TODO_LIST = "UPDATE_TODO_LIST";
 export const UPDATE_CHECKBOX = "UPDATE_CHECKBOX";
 
-export const addTodoList = (payload) => {
+export const addTodoList = (newTodoList) => {
   return {
     type: ADD_TODO_LIST,
-    payload,
+    payload: newTodoList,
   };
 };
 
-export const handleCheckbox = (payload) => {
+export const removeTodoList = (todo) => {
   return {
-    type: UPDATE_CHECKBOX,
+    type: REMOVE_TODO_LIST,
+    payload: todo,
+  };
+};
+
+export const updateTodoList = (payload) => {
+  return {
+    type: UPDATE_TODO_LIST,
     payload,
   };
 };
