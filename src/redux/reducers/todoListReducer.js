8310fee6d2eff077b828f2a76c011360720b394e
Nabil Anugerah <nabilpangestu123@gmail.com>
2023-05-23 08:04:14 +0700

    update reducer

--- src/redux/reducers/todoListReducer.js
+++ src/redux/reducers/todoListReducer.js
@@ -1,59 +1,34 @@
-import { ADD_TODO_LIST, UPDATE_CHECKBOX } from "../actions/todoListAction";
+import {
+  ADD_TODO_LIST,
+  REMOVE_TODO_LIST,
+  UPDATE_TODO_LIST,
+} from "../actions/todoListAction";
 
 const initialState = {
-  todos: [
-    { id: 1, title: "Buy Burger", completed: false },
-    { id: 2, title: "Learn Redux React", completed: false },
-    { id: 3, title: "Go to Bandung", completed: true },
-  ],
+  todos: [],
 };
 
-export function addTodoList(newTodoList) {
-  return {
-    type: ADD_TODO_LIST,
-    payload: newTodoList,
-  };
-}
-
-const todoListReducer = (state = initialState, action) => {
+export const todoListReducer = (state = initialState, action) => {
   switch (action.type) {
     case ADD_TODO_LIST:
       return {
         todos: [...state.todos, action.payload],
       };
-
-    case UPDATE_CHECKBOX:
-      let todoCheckbox = [];
-      state.map((item) => {
-        if (item.id === action.payload) {
-          item.completed = !item.completed;
-        }
-        todoCheckbox.push(item);
-      });
-      return todoCheckbox;
+    case REMOVE_TODO_LIST:
+      return {
+        todos: state.todos.filter((todo) => todo.id !== action.payload.id),
+      };
+    case UPDATE_TODO_LIST:
+      return {
+        todos: state.todos.map((todo) =>
+          todo.id === action.payload.id
+            ? { ...todo, text: action.payload.text }
+            : todo
+        ),
+      };
     default:
       return state;
   }
 };
 
 export default todoListReducer;
-
-/* Test */
-// import { ADD_TODO_LIST } from "../actions/todoListAction";
-
-// const initialState = [
-//   { id: 1, title: "Buy Burger", completed: false },
-//   { id: 2, title: "Learn Redux React", completed: false },
-//   { id: 3, title: "Go to Bandung", completed: true },
-// ];
-
-// export const todoListReducer = (state = initialState, action) => {
-//   switch (action.type) {
-//     case ADD_TODO_LIST:
-//       return [...state, action.payload];
-//     default:
-//       return state;
-//   }
-// };
-
-// export default todoListReducer;
