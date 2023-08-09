fbca97f76d00a7c367d3e6284d8838a6957d3346
Nabil Anugerah <nabilpangestu123@gmail.com>
2023-08-09 11:05:35 +0700

    fix bug

--- src/redux/reducers/todoListReducer.js
+++ src/redux/reducers/todoListReducer.js
@@ -15,6 +15,7 @@ export const todoListReducer = (state = initialState, action) => {
   switch (action.type) {
     case ADD_TODO_LIST:
       return {
+        ...state,
         todos: [...state.todos, action.payload],
       };
     case REMOVE_TODO_LIST:
