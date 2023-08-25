c4e83b0d8c4447e9088a2ce2835fc7eff2e0350f
Nabil Anugerah <nabilpangestu123@gmail.com>
2023-08-25 16:09:40 +0700

    updated position date and time

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -2,15 +2,17 @@ import { FaCalendarAlt } from "react-icons/fa";
 
 const Header = ({ myDateTime }) => {
      return (
-          <div className="text-center sm:text-center">
-               <h1 className="text-violet-600 text-3xl sm:text-4xl font-extrabold">
+          <>
+               <h1 className="text=center text-violet-600 text-3xl sm:text-4xl font-extrabold">
                     What's the plan for today?
                </h1>
-               <div className="flex items-center">
-                    <FaCalendarAlt className="text-slate-500 me-2 mt-2" />
-                    <p className="mt-3 text-md  text-slate-500">{myDateTime}</p>
+               <div className="flex items-center justify-center">
+                    <div className="flex text-center">
+                         <FaCalendarAlt className="text-slate-500 me-2 mt-3" />
+                         <p className="mt-3 text-md  text-slate-500">{myDateTime}</p>
+                    </div>
                </div>
-          </div>
+          </>
      )
 }
 
